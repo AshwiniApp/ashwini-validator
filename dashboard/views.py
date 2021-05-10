ec274e4fb58e1af477f998956da5d43c31ff26f3
ANUJ SINGH <47244602+JackCompass@users.noreply.github.com>
2021-05-10 17:16:09 +0530

    Added html and css files along with js

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render, reverse
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from decouple import config
+import firebase_admin
+from firebase_admin import db
+import json
 
-def home(request):
-	return HttpResponse("Hello world")
+asklepius_obj = firebase_admin.credentials.Certificate(config('CERTIFICATE'))
+default_app = firebase_admin.initialize_app(asklepius_obj, {
+	'databaseURL' : config('DATABASE_URL')
+})
+
+ref = db.reference('users')
+
+def home(request, limit = -1):
+	snapshot = ref.get()
+	patient_data = dict()
+	total_entries = 0
+	result_found = False
+	username = request.user.username
+
+	if (request.method == 'POST'):
+		for patient, patient_details in snapshot.items():
+			if patient_details['regID'] == request.POST(['id']):
+				patient_data[patient] = patient_details
+				result_found = True
+			if patient_details['status'] == 'Pending':
+				total_entries += 1
+		limit = 1 if result_found else 0
+	
+	else:
+		for patient, patient_details in snapshot.items():
+			if patient_details['status'] == 'Pending':
+				patient_data[patient] = patient_details
+				total_entries += 1
+		if limit == -1 or limit > total_entries:
+			limit = total_entries
+	return render(request, 'dashboard/home.html', {
+			'patients_data' : patient_data,
+			'username' : username,
+			'total_entries' : total_entries,
+			'limit' : limit
+	})
