8cc4f8fda9c483a4eb26e78de239d56f9a7a99a7
ANUJ SINGH <47244602+JackCompass@users.noreply.github.com>
2021-05-10 17:18:36 +0530

    Added different routes in the application

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name = 'home'),
+	path('<int:limit>', views.home, name = 'limiter'),
+	path('accepted/<str:id>/<int:limit>', views.accepted, name = 'accepted'),
+	path('rejected/<str:id>/<int:limit>', views.rejected, name = 'rejected'),
 ]
